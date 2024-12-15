c459cda9009a08a758f8fa36ac984f53eb302a26
pettersson932 <144891931+pettersson932@users.noreply.github.com>
2024-12-15 09:45:59 +0100

    x

--- src/components/helloworldFail/HelloworldFail.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const HelloWorldFail = ({ name }) => {
-  return <h1>Hello, {name}!</h1>;
-};
-
-export default HelloWorldFail;
--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,12 @@ export default defineConfig({
   test: {
     environment: "jsdom", // Simulate browser environment with jsdom
     globals: true, // Global variables like `describe`, `it`, `expect`, etc.
-    setupFiles: "./src/setupTests.js", // Path to setup file (for jest-dom and other setups)
+    setupFiles: "./setupTests.js", // Path to setup file (for jest-dom and other setups)
     coverage: {
-      reporter: ["text", "lcov"], // Text output in terminal, lcov for HTML report
+      provider: "v8",
+      reporter: ["text", "json", "lcov", "json-summary"], // Text output in terminal, lcov for HTML report
+      include: ["src/**/*.js", "src/**/*.jsx"],
+      exclude: ["src/main.jsx", "src/App.jsx"],
       reportsDirectory: "./coverage", // Directory where coverage reports will be stored
     },
   },
